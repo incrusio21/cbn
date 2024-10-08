14170d2eebc5e8cbacc6f999975311e88a0722c1
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2024-10-08 02:30:16 +0000

    opt (stock controller): update get_sl_entries d init

--- cbn/__init__.py
+++ cbn/__init__.py
@@ -1,1 +1,44 @@
 __version__ = "0.0.1"
+
+import frappe
+from frappe.utils.data import flt
+
+from erpnext.accounts.utils import get_fiscal_year
+from erpnext.controllers.stock_controller import StockController
+
+def get_sl_entries(self, d, args):
+    sl_dict = frappe._dict(
+        {
+            "item_code": d.get("item_code", None),
+            "warehouse": d.get("warehouse", None),
+            "serial_and_batch_bundle": d.get("serial_and_batch_bundle"),
+            "custom_batch": d.get("custom_batch"),
+            "posting_date": self.posting_date,
+            "posting_time": self.posting_time,
+            "fiscal_year": get_fiscal_year(self.posting_date, company=self.company)[0],
+            "voucher_type": self.doctype,
+            "voucher_no": self.name,
+            "voucher_detail_no": d.name,
+            "actual_qty": (self.docstatus == 1 and 1 or -1) * flt(d.get("stock_qty")),
+            "stock_uom": frappe.get_cached_value(
+                "Item", args.get("item_code") or d.get("item_code"), "stock_uom"
+            ),
+            "incoming_rate": 0,
+            "company": self.company,
+            "project": d.get("project") or self.get("project"),
+            "is_cancelled": 1 if self.docstatus == 2 else 0,
+        }
+    )
+
+    sl_dict.update(args)
+    self.update_inventory_dimensions(d, sl_dict)
+
+    if self.docstatus == 2:
+        # To handle denormalized serial no records, will br deprecated in v16
+        for field in ["serial_no", "batch_no"]:
+            if d.get(field):
+                sl_dict[field] = d.get(field)
+
+    return sl_dict
+
+StockController.get_sl_entries = get_sl_entries
