b92f1a442a95d12ffecf646de873f7fff8df5e02
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2024-11-15 06:06:54 +0000

    refa(quality inspection): gabungkan field start & end untuk setiap operation

--- cbn/cbn/custom/quality_inspection.js
+++ cbn/cbn/custom/quality_inspection.js
@@ -2,6 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Quality Inspection", {
+    refresh(frm){
+        frm.trigger("update_field")
+    },
     reference_name(frm){
         if(frm.doc.reference_type == "Job Card" && frm.doc.reference_name){
             frappe.call({
@@ -12,15 +15,39 @@ frappe.ui.form.on("Quality Inspection", {
                 callback: (r) => {
                     let data = r.message
                     if (data) {
+                        frm.set_value("custom_operation", data.operation);
                         frm.set_value("custom_batch_size", data.qty);
                         frm.set_value("custom_line_produksi", data.custom_line_produksi);
-                        frm.set_value("custom_start_filling", data.actual_start_date);
-                        frm.set_value("custom_end_filling", data.actual_end_date);
-                        frm.set_value("custom_start_packing", data.actual_start_date);
-                        frm.set_value("custom_end_filling", data.actual_end_date);
+                        frm.set_value("custom_start_time", data.actual_start_date);
+                        frm.set_value("custom_end_time", data.actual_end_date);
                     }
                 }
             })
+        }else{
+            frm.set_value("custom_operation", "");
         }
-    }
+    },
+    custom_operation(frm){
+        frm.trigger("update_field")
+    },
+    update_field(frm){
+        if(!frm.doc.custom_operation) return
+
+        var field_label_map = {};
+        $.each(["custom_start_time", "custom_end_time"], function (i, fname) {
+			var docfield = frappe.meta.docfield_map["Quality Inspection"][fname];
+			if (docfield) {
+				var label = __(docfield.label || "", null, docfield.parent).replace(
+					/\bTime\b/g,
+					""
+				); // eslint-disable-line
+
+                field_label_map[fname] = label + frm.doc.custom_operation;
+			}
+		});
+
+		$.each(field_label_map, function (fname, label) {
+			frm.fields_dict[fname].set_label(label);
+		});
+    }    
 })
