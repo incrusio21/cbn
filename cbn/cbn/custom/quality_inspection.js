0fe9048d8ba0283d7bf91caa35fd9ad3ec5d0f88
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2024-11-15 03:56:03 +0000

    refa(quality inspection): get_job_card menggunakan whitelist

--- cbn/cbn/custom/quality_inspection.js
+++ cbn/cbn/custom/quality_inspection.js
@@ -4,11 +4,21 @@
 frappe.ui.form.on("Quality Inspection", {
     reference_name(frm){
         if(frm.doc.reference_type == "Job Card" && frm.doc.reference_name){
-            frappe.db.get_value("Job Card", frm.doc.reference_name, ["name", "actual_start_date", "actual_end_date"], 
-                (r) => {
-                if (r && r.name) {
-                    frm.set_value("custom_start_filling", r.actual_start_date);
-                    frm.set_value("custom_end_filling", r.actual_end_date);
+            frappe.call({
+                method: "cbn.cbn.custom.quality_inspection.get_job_card",
+                args: {
+                    job_card: frm.doc.reference_name
+                },
+                callback: (r) => {
+                    let data = r.message
+                    if (data) {
+                        frm.set_value("custom_batch_size", r.qty);
+                        frm.set_value("custom_line_produksi", r.custom_line_produksi);
+                        frm.set_value("custom_start_filling", r.actual_start_date);
+                        frm.set_value("custom_end_filling", r.actual_end_date);
+                        frm.set_value("custom_start_packing", r.actual_start_date);
+                        frm.set_value("custom_end_filling", r.actual_end_date);
+                    }
                 }
             })
         }
--- cbn/cbn/custom/quality_inspection.py
+++ cbn/cbn/custom/quality_inspection.py
@@ -8,3 +8,25 @@ def set_job_card_bm(self, method=None):
         return
     
     self.custom_batch = frappe.get_value(self.reference_type, self.reference_name, "custom_batch")
+
+@frappe.whitelist()
+def get_job_card(job_card):
+    jc = frappe.qb.DocType("Job Card")
+    wo = frappe.qb.DocType("Work Order")
+
+    query = (
+        frappe.qb.from_(jc)
+        .inner_join(wo)
+        .on(jc.work_order == wo.name)
+        .select(
+            jc.actual_start_date,
+            jc.actual_end_date,
+            wo.qty,
+            wo.custom_line_produksi,
+        )
+        .where(jc.name == job_card)
+    )
+
+    data = query.run(as_dict=True)
+
+    return data
