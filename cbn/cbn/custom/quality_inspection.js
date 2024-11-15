db6f98a1bee538a914f32bd5efc8fb1f3ea8528d
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2024-11-15 04:01:49 +0000

    refa(quality inspection): ubah var r menjadi data

--- cbn/cbn/custom/quality_inspection.js
+++ cbn/cbn/custom/quality_inspection.js
@@ -12,12 +12,12 @@ frappe.ui.form.on("Quality Inspection", {
                 callback: (r) => {
                     let data = r.message
                     if (data) {
-                        frm.set_value("custom_batch_size", r.qty);
-                        frm.set_value("custom_line_produksi", r.custom_line_produksi);
-                        frm.set_value("custom_start_filling", r.actual_start_date);
-                        frm.set_value("custom_end_filling", r.actual_end_date);
-                        frm.set_value("custom_start_packing", r.actual_start_date);
-                        frm.set_value("custom_end_filling", r.actual_end_date);
+                        frm.set_value("custom_batch_size", data.qty);
+                        frm.set_value("custom_line_produksi", data.custom_line_produksi);
+                        frm.set_value("custom_start_filling", data.actual_start_date);
+                        frm.set_value("custom_end_filling", data.actual_end_date);
+                        frm.set_value("custom_start_packing", data.actual_start_date);
+                        frm.set_value("custom_end_filling", data.actual_end_date);
                     }
                 }
             })
